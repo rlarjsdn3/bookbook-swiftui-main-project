0fb06e6fada8d51848c38e43a4e51191a02f979f
Zizi_Kim <rlarjsdn3@naver.com>
2023-03-13 18:14:33 +0900

    [add]: 프로젝트 생성

---
+++ BookyBooky/BookyBooky/BookyBookyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookyBookyApp.swift
+//  BookyBooky
+//
+//  Created by 김건우 on 2023/03/13.
+//
+
+import SwiftUI
+
+@main
+struct BookyBookyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BookyBooky/BookyBooky/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  BookyBooky
+//
+//  Created by 김건우 on 2023/03/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
