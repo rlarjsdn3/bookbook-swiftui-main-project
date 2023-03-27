aa39c17d00f02dd9fd37cf8ea89e6bbbd5868279
Zizi_Kim <rlarjsdn3@naver.com>
2023-03-27 09:34:24 +0900

    [file]: RoundedTabView 파일 추가

---
+++ BookyBooky/BookyBooky/Views/TabView/RoundedTabView.swift
@@ -0,0 +1,20 @@
+//
+//  RoundedTabView.swift
+//  BookyBooky
+//
+//  Created by 김건우 on 2023/03/27.
+//
+
+import SwiftUI
+
+struct RoundedTabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RoundedTabView_Previews: PreviewProvider {
+    static var previews: some View {
+        RoundedTabView()
+    }
+}
