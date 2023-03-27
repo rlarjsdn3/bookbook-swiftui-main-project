40d10910defb7224a01ca4e0c714cb8a77bcd915
Zizi_Kim <rlarjsdn3@naver.com>
2023-03-27 10:22:05 +0900

    [add]: TabItem 파일 내용 수정

--- BookyBooky/BookyBooky/Models/TabItem.swift
+++ BookyBooky/BookyBooky/Models/TabItem.swift
@@ -5,6 +5,7 @@
 //  Created by 김건우 on 2023/03/27.
 //
 
+import SwiftUI
 import Foundation
 
 enum TabItem: CaseIterable {
@@ -49,6 +50,20 @@ enum TabItem: CaseIterable {
             return "books.vertical.fill"
         case .analysis:
             return "chart.bar.fill"
+        }
+    }
+    
+    var tabColor: Color {
+        switch self {
+        case .home, .search, .bookShelf, .analysis:
+            return .gray
+        }
+    }
+    
+    var tabColorPressed: Color {
+        switch self {
+        case .home, .search, .bookShelf, .analysis:
+            return .black
         }
     }
 }
