0e9a34f7d695e606c8793c68495c3526720473be
Zizi_Kim <rlarjsdn3@naver.com>
2023-03-27 10:05:46 +0900

    [add]: TabItem 파일 내용 수정

--- BookyBooky/BookyBooky/Models/TabItem.swift
+++ BookyBooky/BookyBooky/Models/TabItem.swift
@@ -6,3 +6,49 @@
 //
 
 import Foundation
+
+enum TabItem: CaseIterable {
+    case home
+    case search
+    case bookShelf
+    case analysis
+    
+    var tabName: String {
+        switch self {
+        case .home:
+            return "홈"
+        case .search:
+            return "검색"
+        case .bookShelf:
+            return "책장"
+        case .analysis:
+            return "분석"
+        }
+    }
+    
+    var tabIcon: String {
+        switch self {
+        case .home:
+            return "house"
+        case .search:
+            return "magnifyingglass.circle"
+        case .bookShelf:
+            return "books.vertical"
+        case .analysis:
+            return "chart.bar"
+        }
+    }
+    
+    var tabIconPressed: String {
+        switch self {
+        case .home:
+            return "house.fill"
+        case .search:
+            return "magnifyingglass.circle.fill"
+        case .bookShelf:
+            return "books.vertical.fill"
+        case .analysis:
+            return "chart.bar.fill"
+        }
+    }
+}
