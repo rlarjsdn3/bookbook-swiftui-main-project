4bbbeb80fc4a61bfdd64cc9bb6dcb7f2641300a0
Zizi_Kim <rlarjsdn3@naver.com>
2023-03-27 23:10:44 +0900

    [file]: HomeView 파일 추가

---
+++ BookyBooky/BookyBooky/Views/HomeView/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  BookyBooky
+//
+//  Created by 김건우 on 2023/03/27.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
