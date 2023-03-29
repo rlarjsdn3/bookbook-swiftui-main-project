f9fb650800c55da405802049974123a7c33663bd
Zizi_Kim <rlarjsdn3@naver.com>
2023-03-29 19:04:58 +0900

    [ file]: SearchSheetTextFieldView 파일 추가

---
+++ BookyBooky/BookyBooky/Views/SearchView/SearchSheetTextFieldView.swift
@@ -0,0 +1,61 @@
+//
+//  SearchSheetTextFieldView.swift
+//  BookyBooky
+//
+//  Created by 김건우 on 2023/03/29.
+//
+
+import SwiftUI
+
+struct SearchSheetTextFieldView: View {
+    @EnvironmentObject var viewModel: ViewModel
+    @Binding var query: String
+    
+    var body: some View {
+        HStack {
+            HStack {
+                Image(systemName: "magnifyingglass")
+                    .foregroundColor(.gray)
+                
+                TextField("제목 / 저자 검색", text: $query)
+                    .submitLabel(.search)
+                    .onSubmit {
+                        requestBookSearch(query: query)
+                    }
+                
+                if !query.isEmpty {
+                    Button {
+                        query = ""
+                    } label: {
+                        Image(systemName: "xmark.circle.fill")
+                            .foregroundColor(.gray)
+                    }
+                }
+            }
+            .padding(.vertical, 15)
+            .padding(.horizontal, 10)
+            .background(Color("Background"))
+            .cornerRadius(15)
+            .padding()
+            
+            Button {
+                requestBookSearch(query: query)
+            } label: {
+                Text("검색")
+            }
+            .padding(.trailing)
+        }
+    }
+    
+    func requestBookSearch(query: String) {
+        viewModel.requestBookSearchAPI(search: query)
+        self.query = ""
+    }
+}
+
+struct SearchSheetTextFieldView_Previews: PreviewProvider {
+    static var previews: some View {
+        SearchSheetTextFieldView(query: .constant(""))
+            .environmentObject(ViewModel())
+    }
+}
