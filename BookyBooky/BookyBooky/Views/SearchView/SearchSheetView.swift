599e580c2ff7287a08755a24baadca48222ef1d8
Zizi_Kim <rlarjsdn3@naver.com>
2023-03-29 14:30:24 +0900

    [file]: SearchSheetView 파일 추가

---
+++ BookyBooky/BookyBooky/Views/SearchView/SearchSheetView.swift
@@ -0,0 +1,20 @@
+//
+//  SearchSheetView.swift
+//  BookyBooky
+//
+//  Created by 김건우 on 2023/03/29.
+//
+
+import SwiftUI
+
+struct SearchSheetView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SearchSheetView_Previews: PreviewProvider {
+    static var previews: some View {
+        SearchSheetView()
+    }
+}
