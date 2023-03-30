f7ae5b76f26efc43e35d35e8743525f907d3ae37
Zizi_Kim <rlarjsdn3@naver.com>
2023-03-30 23:30:42 +0900

    [file]: SearchCategoryView 파일 추가

---
+++ BookyBooky/BookyBooky/Views/SearchView/SearchCategoryView.swift
@@ -0,0 +1,20 @@
+//
+//  SearchCategoryView.swift
+//  BookyBooky
+//
+//  Created by 김건우 on 2023/03/30.
+//
+
+import SwiftUI
+
+struct SearchCategoryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SearchCategoryView_Previews: PreviewProvider {
+    static var previews: some View {
+        SearchCategoryView()
+    }
+}
