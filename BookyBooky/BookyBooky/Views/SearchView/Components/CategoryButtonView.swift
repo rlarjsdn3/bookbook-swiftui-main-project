4f2943f3d4fbce6422e2b4e542d18fe27a6cfd64
Zizi_Kim <rlarjsdn3@naver.com>
2023-03-31 00:05:07 +0900

    [file]: CategoryButtonView 파일 추가

---
+++ BookyBooky/BookyBooky/Views/SearchView/Components/CategoryButtonView.swift
@@ -0,0 +1,20 @@
+//
+//  CategoryButtonView.swift
+//  BookyBooky
+//
+//  Created by 김건우 on 2023/03/31.
+//
+
+import SwiftUI
+
+struct CategoryButtonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CategoryButtonView_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryButtonView()
+    }
+}
