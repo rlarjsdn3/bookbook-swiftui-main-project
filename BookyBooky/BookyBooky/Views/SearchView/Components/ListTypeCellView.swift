bc58aa0900bc67011694aaacfe359cb251dd913d
Zizi_Kim <rlarjsdn3@naver.com>
2023-03-31 20:02:50 +0900

    [file]: ListTypeCellView 파일 추가

---
+++ BookyBooky/BookyBooky/Views/SearchView/Components/ListTypeCellView.swift
@@ -0,0 +1,20 @@
+//
+//  ListTypeCellView.swift
+//  BookyBooky
+//
+//  Created by 김건우 on 2023/03/31.
+//
+
+import SwiftUI
+
+struct ListTypeCellView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ListTypeCellView_Previews: PreviewProvider {
+    static var previews: some View {
+        ListTypeCellView()
+    }
+}
