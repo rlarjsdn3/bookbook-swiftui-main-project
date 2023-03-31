9514dace38a9b90cb5f56a691e06de969bd47133
Zizi_Kim <rlarjsdn3@naver.com>
2023-03-31 10:35:18 +0900

    [file]: SearchSheetScrollView 파일 추가

---
+++ BookyBooky/BookyBooky/Views/SearchSheetView/SearchSheetScrollView.swift
@@ -0,0 +1,20 @@
+//
+//  SearchSheetScrollView.swift
+//  BookyBooky
+//
+//  Created by 김건우 on 2023/03/31.
+//
+
+import SwiftUI
+
+struct SearchSheetScrollView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SearchSheetScrollView_Previews: PreviewProvider {
+    static var previews: some View {
+        SearchSheetScrollView()
+    }
+}
