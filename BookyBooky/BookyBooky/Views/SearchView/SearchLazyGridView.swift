03a1af24cee2f742e3b7f80c3def7e5c596d94dd
Zizi_Kim <rlarjsdn3@naver.com>
2023-03-31 21:18:15 +0900

    [file]: SearhLazyGridView 파일 추가

---
+++ BookyBooky/BookyBooky/Views/SearchView/SearchLazyGridView.swift
@@ -0,0 +1,20 @@
+//
+//  SearchLazyGridView.swift
+//  BookyBooky
+//
+//  Created by 김건우 on 2023/03/31.
+//
+
+import SwiftUI
+
+struct SearchLazyGridView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SearchLazyGridView_Previews: PreviewProvider {
+    static var previews: some View {
+        SearchLazyGridView()
+    }
+}
