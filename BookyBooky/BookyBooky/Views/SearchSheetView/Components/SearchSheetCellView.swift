8382a7ea515219eae7fc2ab512f34abcad09f7e7
Zizi_Kim <rlarjsdn3@naver.com>
2023-04-01 11:09:07 +0900

    [file]: SearchSheetCellView 파일 추가

---
+++ BookyBooky/BookyBooky/Views/SearchSheetView/Components/SearchSheetCellView.swift
@@ -0,0 +1,20 @@
+//
+//  SearchSheetCellView.swift
+//  BookyBooky
+//
+//  Created by 김건우 on 2023/04/01.
+//
+
+import SwiftUI
+
+struct SearchSheetCellView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SearchSheetCellView_Previews: PreviewProvider {
+    static var previews: some View {
+        SearchSheetCellView()
+    }
+}
