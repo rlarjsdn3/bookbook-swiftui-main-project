02630a3774d0f7c198b8abcd9aca71bad8627ca2
Zizi_Kim <rlarjsdn3@naver.com>
2023-04-03 12:58:23 +0900

    [file]: SearchDetailTitleView 파일 추가

---
+++ BookyBooky/BookyBooky/Views/SearchDetailView/SearchDetailTitleView.swift
@@ -0,0 +1,60 @@
+//
+//  BookDetailTitleView.swift
+//  BookyBooky
+//
+//  Created by 김건우 on 2023/04/03.
+//
+
+import SwiftUI
+
+struct SearchDetailTitleView: View {
+    let bookInfo: BookDetail.Item
+    
+    var body: some View {
+        HStack {
+            VStack(alignment: .leading, spacing: 5) {
+                Text(bookInfo.originalTitle)
+                    .font(.title)
+                    .fontWeight(.bold)
+                    .minimumScaleFactor(0.8)
+                    .lineLimit(1)
+                
+                HStack(spacing: 2) {
+                    Text(bookInfo.authorInfo)
+                    
+                    Text("・")
+                    
+                    Text(bookInfo.publisher)
+                }
+                .font(.headline)
+                .foregroundColor(.secondary)
+                
+                Spacer()
+            }
+            
+            Spacer()
+            
+            // '좋아요' 버튼은 미완성입니다. (디자인, 기능 등)
+            Button {
+                
+            } label: {
+                Image(systemName: "heart.fill")
+                    .foregroundColor(.white)
+                    .padding()
+                    .background(.pink)
+                    .clipShape(Circle())
+            }
+
+        }
+        .frame(height: 60)
+        .padding()
+        
+        Spacer()
+    }
+}
+
+struct BookDetailTitleView_Previews: PreviewProvider {
+    static var previews: some View {
+        SearchDetailTitleView(bookInfo: BookDetail.Item.preview[0])
+    }
+}
