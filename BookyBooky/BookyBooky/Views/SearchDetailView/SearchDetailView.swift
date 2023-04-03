1c8f10b3a83930d3cba1e69ad2fac3341205f5b9
Zizi_Kim <rlarjsdn3@naver.com>
2023-04-03 13:48:35 +0900

    [add]: 도서 상세 뷰 버튼 추가(기능 동작X)

--- BookyBooky/BookyBooky/Views/SearchDetailView/SearchDetailView.swift
+++ BookyBooky/BookyBooky/Views/SearchDetailView/SearchDetailView.swift
@@ -36,6 +36,40 @@ struct SearchDetailView: View {
                     SearchDetailDescriptionView(bookInfo: bookInfo)
                     
                     Spacer()
+                    
+                    VStack {
+                        Text("도서 DB 제공 : 알라딘 인터넷서점(www.aladin.co.kr)")
+                            .font(.caption)
+                        
+                        HStack {
+                            Button {
+                                
+                            } label: {
+                                Text("돌아가기")
+                                    .font(.title3)
+                                    .fontWeight(.bold)
+                                    .foregroundColor(.black)
+                                    .frame(height: 55)
+                                    .frame(maxWidth: .infinity)
+                                    .background(.gray.opacity(0.2))
+                                    .cornerRadius(15)
+                            }
+                            
+                            Button {
+                                
+                            } label: {
+                                Text("추가하기")
+                                    .font(.title3)
+                                    .fontWeight(.bold)
+                                    .foregroundColor(.white)
+                                    .frame(height: 55)
+                                    .frame(maxWidth: .infinity)
+                                    .background(.pink)
+                                    .cornerRadius(15)
+                            }
+                        }
+                        .padding([.horizontal, .bottom])
+                    }
                 }
             } else {
                 // 로딩 뷰 따로 출력하기
