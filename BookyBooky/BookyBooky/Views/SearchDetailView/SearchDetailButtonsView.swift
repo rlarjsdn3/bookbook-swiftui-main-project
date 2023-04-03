1bcc0a24f4f31d84d73a2d0f2ca153319f82f035
Zizi_Kim <rlarjsdn3@naver.com>
2023-04-03 15:37:34 +0900

    [file]: SearchDetailButtonsView 파일 추가

---
+++ BookyBooky/BookyBooky/Views/SearchDetailView/SearchDetailButtonsView.swift
@@ -0,0 +1,20 @@
+//
+//  SearchDetailButtonsView.swift
+//  BookyBooky
+//
+//  Created by 김건우 on 2023/04/03.
+//
+
+import SwiftUI
+
+struct SearchDetailButtonsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SearchDetailButtonsView_Previews: PreviewProvider {
+    static var previews: some View {
+        SearchDetailButtonsView()
+    }
+}
