3763cda97c87112062ea56665e963da097c2666f
Zizi_Kim <rlarjsdn3@naver.com>
2023-04-05 18:35:13 +0900

    [add]: Realm 초기화 코드 추가

--- BookyBooky/BookyBooky/BookyBookyApp.swift
+++ BookyBooky/BookyBooky/BookyBookyApp.swift
@@ -10,6 +10,10 @@ import SwiftUI
 @main
 struct BookyBookyApp: App {
     
+    // MARK: - CONSTATNT PROPERTIES
+    
+    let realmManager = RealmManager.shared
+    
     // MARK: - WRAPPER PROPERTIES
     
     @StateObject var aladinAPIManager = AladinAPIManager()
@@ -18,13 +22,18 @@ struct BookyBookyApp: App {
     
     var body: some Scene {
         WindowGroup {
-            ContentView()
-                .onAppear {
-                    for type in ListType.allCases {
-                        aladinAPIManager.requestBookListAPI(type: type)
+            if let realm = realmManager.realm {
+                ContentView()
+                    .onAppear {
+                        for type in ListType.allCases {
+                            aladinAPIManager.requestBookListAPI(type: type)
+                        }
                     }
-                }
-                .environmentObject(aladinAPIManager)
+                    .environmentObject(aladinAPIManager)
+                    .environment(\.realm, realm)
+            } else {
+                // 스플래쉬 뷰ㅋ
+            }
         }
     }
 }
--- BookyBooky/BookyBooky/RealmManager/RealmManager.swift
+++ BookyBooky/BookyBooky/RealmManager/RealmManager.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+import RealmSwift
+
+class RealmManager {
+    let realm = openLocalRealm()
+    
+    static let shared = RealmManager()
+    
+    private init() { }
+    
+    static func openLocalRealm() -> Realm? {
+        do {
+            let config = Realm.Configuration(
+                schemaVersion: 0,
+                deleteRealmIfMigrationNeeded: true)
+            print("Realm DB 저장소의 위치: \(config.fileURL!)")
+            
+            return try Realm(configuration: config)
+        } catch {
+            print("Realm DB 초기화 예외: \(error)")
+        }
+        
+        return nil
+    }
+}
