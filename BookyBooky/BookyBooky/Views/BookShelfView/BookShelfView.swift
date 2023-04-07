c4ba0f918b73bd762e9dc1bd91478407c896ecbb
Zizi_Kim <rlarjsdn3@naver.com>
2023-04-07 14:22:23 +0900

    [file]: BookShelfView 파일 추가

---
+++ BookyBooky/BookyBooky/Views/BookShelfView/BookShelfView.swift
@@ -0,0 +1,20 @@
+//
+//  BookShelfView.swift
+//  BookyBooky
+//
+//  Created by 김건우 on 2023/04/07.
+//
+
+import SwiftUI
+
+struct BookShelfView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct BookShelfView_Previews: PreviewProvider {
+    static var previews: some View {
+        BookShelfView()
+    }
+}
