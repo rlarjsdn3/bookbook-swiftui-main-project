cf863f70e01c9c5524db0d5c56c1808794e1ec03
Zizi_Kim <rlarjsdn3@naver.com>
2023-04-09 10:26:06 +0900

    [file]: FavoriteBooksView 파일 추가

---
+++ BookyBooky/BookyBooky/Views/FavoriteBooksView/FavoriteBooksView.swift
@@ -0,0 +1,20 @@
+//
+//  FavoriteBooksView.swift
+//  BookyBooky
+//
+//  Created by 김건우 on 2023/04/09.
+//
+
+import SwiftUI
+
+struct FavoriteBooksView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FavoriteBooksView_Previews: PreviewProvider {
+    static var previews: some View {
+        FavoriteBooksView()
+    }
+}
