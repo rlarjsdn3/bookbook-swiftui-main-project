6cc58572a1399f8e1a1e0ed7f1dd288e597ded65
Zizi_Kim <rlarjsdn3@naver.com>
2023-04-10 10:13:46 +0900

    [refactor]: 코드 리팩토링

--- BookyBooky/BookyBooky/Views/FavoriteBooksView/FavoriteBooksView.swift
+++ BookyBooky/BookyBooky/Views/FavoriteBooksView/FavoriteBooksView.swift
@@ -10,14 +10,6 @@ import AlertToast
 import RealmSwift
 
 struct FavoriteBooksView: View {
-    
-    // MARK: - CONSTANT PROPERTIES
-    
-    let coulmns = [
-        GridItem(.flexible()),
-        GridItem(.flexible())
-    ]
-    
     // MARK: - WRAPPER PROPERTIES
     
     @ObservedResults(FavoriteBook.self) var favoriteBooks
@@ -29,33 +21,6 @@ struct FavoriteBooksView: View {
     @State var isPresentingShowAll = false
     
     @FocusState var focusedField: Bool
-    
-    // MARK: - COMPUTED PROPERTIES
-    
-    var sortedFavoritesBooks: [FavoriteBook] {
-        switch selectedSort {
-        // 최근 추가된 순으로 정렬
-        case .latestOrder:
-            return favoriteBooks.reversed()
-        // 제목 오름차순으로 정렬
-        case .titleOrder:
-            return favoriteBooks.sorted { $0.title < $1.title }
-        // 판매 포인트 내림차순으로 정렬
-        case .sellingPointOrder:
-            return favoriteBooks.sorted { $0.salesPoint.toInteger > $1.salesPoint.toInteger }
-        }
-    }
-    
-    var filteredFavroriteBooks: [FavoriteBook] {
-        if !searchQuery.isEmpty {
-            let filteredFavoriteBooks = sortedFavoritesBooks.filter {
-                $0.title.contains(searchQuery) || $0.author.contains(searchQuery)
-            }
-            return filteredFavoriteBooks
-        } else {
-            return sortedFavoritesBooks
-        }
-    }
     
     // MARK: - BODY
     
@@ -69,19 +34,10 @@ struct FavoriteBooksView: View {
                     scrollProxy: scrollProxy
                 )
 
-                if !filteredFavroriteBooks.isEmpty {
-                    ScrollView {
-                        LazyVGrid(columns: coulmns) {
-                            ForEach(filteredFavroriteBooks) { favoriteBook in
-                                FavoriteBookCellView(favoriteBook: favoriteBook)
-                            }
-                        }
-                        .id("Scroll_To_Top")
-                        .padding(.horizontal, 10)
-                    }
-                } else {
-                    noResultLabel
-                }
+                FavoriteBooksScrollView(
+                    selectedSort: $selectedSort,
+                    searchQuery: $searchQuery
+                )
             }
             .overlay(alignment: .bottom) {
                     Button {
@@ -109,31 +65,7 @@ struct FavoriteBooksView: View {
 // MARK: - EXTENSIONS
 
 extension FavoriteBooksView {
-    var searchButton: some View {
-        Button {
-            hideKeyboard()
-        } label: {
-            Text("검색")
-        }
-        .padding(.horizontal)
-    }
     
-    var noResultLabel: some View {
-        VStack {
-            VStack {
-                Text("결과 없음")
-                    .font(.title2)
-                    .fontWeight(.bold)
-                    .frame(maxHeight: .infinity)
-                
-                Text("새로운 검색을 시도하십시오.")
-                    .font(.headline)
-                    .foregroundColor(.secondary)
-            }
-            .frame(height: 50)
-        }
-        .frame(maxHeight: .infinity)
-    }
 }
 
 // MARK: - PREVIEWS
