71a5a8cdc36151b4ee816e9eb835848faa582454
Zizi_Kim <rlarjsdn3@naver.com>
2023-04-12 21:45:32 +0900

    [file]: FavoriteBookCellViewType 파일 추가

---
+++ BookyBooky/BookyBooky/Enums/FavoriteBookCellViewType.swift
@@ -0,0 +1,13 @@
+//
+//  FavoriteBookCellViewType.swift
+//  BookyBooky
+//
+//  Created by 김건우 on 2023/04/12.
+//
+
+import Foundation
+
+enum FavoriteBookCellViewType {
+    case sheet
+    case navigationStack
+}
