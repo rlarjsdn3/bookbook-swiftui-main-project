0d64df925213bcddb2067e3fd20d2af832502dd3
Zizi_Kim <rlarjsdn3@naver.com>
2023-04-12 22:12:30 +0900

    [add]: 주석 추가

--- BookyBooky/BookyBooky/Extensions/UINavigationControllerExtension.swift
+++ BookyBooky/BookyBooky/Extensions/UINavigationControllerExtension.swift
@@ -7,13 +7,14 @@
 
 import UIKit
 
+/// (코드 출처: https://url.kr/jlx6c7)
 extension UINavigationController: UIGestureRecognizerDelegate {
     override open func viewDidLoad() {
         super.viewDidLoad()
         interactivePopGestureRecognizer?.delegate = self
     }
 
-    /// NavigaionStack에서 '뒤로 가기'버튼을 커스텀하더라도, 스와이프 제스처가 동작하도록 하는 함수입니다. (코드 출처: https://url.kr/jlx6c7)
+    /// NavigaionStack에서 '뒤로 가기'버튼을 커스텀하더라도, 스와이프 제스처가 동작하도록 하는 함수입니다.
     public func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
         return viewControllers.count > 1
     }
