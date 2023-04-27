e20528474fdd94389342c53f62213e3cf51d85f7
Zizi_Kim <rlarjsdn3@naver.com>
2023-04-27 20:48:01 +0900

    [file]: HomeTargetBookDetailView 파일 추가

---
+++ BookyBooky/BookyBooky/Views/HomeView/HomeTargetBookDetailView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeTargetBookDetailView.swift
+//  BookyBooky
+//
+//  Created by 김건우 on 2023/04/27.
+//
+
+import SwiftUI
+
+struct HomeTargetBookDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomeTargetBookDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeTargetBookDetailView()
+    }
+}
