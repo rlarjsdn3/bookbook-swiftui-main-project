96f68c2c7ced20aebe97646c68c89f5b9205258e
Zizi_Kim <rlarjsdn3@naver.com>
2023-05-01 17:45:15 +0900

    [add]: 목표 도서 삭제 기능 추가

--- BookyBooky/BookyBooky/Views/TargetBookDetailView/TargetBookDetailHeaderView.swift
+++ BookyBooky/BookyBooky/Views/TargetBookDetailView/TargetBookDetailHeaderView.swift
@@ -11,6 +11,8 @@ import RealmSwift
 struct TargetBookDetailHeaderView: View {
     @Environment(\.dismiss) var dismiss
     
+    @ObservedResults(CompleteTargetBook.self) var completeTargetBooks
+    
     let targetBook: CompleteTargetBook
     
     var body: some View {
@@ -36,8 +38,32 @@ struct TargetBookDetailHeaderView: View {
                 
                 Spacer()
                 
-                Button {
-                    
+                Menu {
+                    Section {
+                        Button {
+                            // do something...
+                        } label: {
+                            Label("편집", systemImage: "square.and.pencil")
+                        }
+
+                        Button(role: .destructive) {
+                            // 리팩토링 필요
+                            do {
+                                if let object = completeTargetBooks.filter("isbn13 == %@", targetBook.isbn13).first {
+                                    
+                                    $completeTargetBooks.remove(object)
+                                }
+                            } catch let error as NSError {
+                                print("error - \(error.localizedDescription)")
+                            }
+                            dismiss()
+                        } label: {
+                            Label("삭제", systemImage: "trash")
+                        }
+                    } header: {
+                        Text("도서 편집")
+                    }
+
                 } label: {
                     Image(systemName: "ellipsis")
                         .navigationBarItemStyle()
