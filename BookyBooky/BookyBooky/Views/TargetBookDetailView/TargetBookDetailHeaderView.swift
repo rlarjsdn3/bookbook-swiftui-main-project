93f47a4d0f896b2952184f57446f86caaf4df044
Zizi_Kim <rlarjsdn3@naver.com>
2023-05-02 22:05:56 +0900

    [refactor]: 코드 리팩토링

--- BookyBooky/BookyBooky/Views/TargetBookDetailView/TargetBookDetailHeaderView.swift
+++ BookyBooky/BookyBooky/Views/TargetBookDetailView/TargetBookDetailHeaderView.swift
@@ -21,67 +21,70 @@ struct TargetBookDetailHeaderView: View {
     var body: some View {
         HStack {
             Spacer()
-            ZStack {
-                Text("도서 정보")
-                    .navigationTitleStyle()
-                    .opacity(scrollYOffset > 29 ? 0 : 1)
-                
-                Text(title)
-                    .frame(width: mainScreen.width * 0.65)
-                    .lineLimit(1)
-                    .truncationMode(.middle)
-                    .navigationTitleStyle()
-                    .opacity(scrollYOffset > 30 ? 1 : 0)
-            }
+            
+            navigationTitle
             
             Spacer()
         }
         .overlay {
-            HStack {
-                Button {
-                    dismiss()
-                } label: {
-                    Image(systemName: "chevron.left")
-                        .navigationBarItemStyle()
-                }
-                
-                Spacer()
-                
-                Menu {
-                    Section {
-                        Button {
-                            // do something...
-                        } label: {
-                            Label("편집", systemImage: "square.and.pencil")
-                        }
-
-                        Button(role: .destructive) {
-                            // 리팩토링 필요
-                            dismiss()
-                            do {
-                                if let object = completeTargetBooks.filter("isbn13 == %@", targetBook.isbn13).first {
-                                    $completeTargetBooks.remove(object)
-                                }
-                            } catch let error as NSError {
-                                print("error - \(error.localizedDescription)")
-                            }
-                        } label: {
-                            Label("삭제", systemImage: "trash")
-                        }
-                    } header: {
-                        Text("도서 편집")
-                    }
-
-                } label: {
-                    Image(systemName: "ellipsis")
-                        .navigationBarItemStyle()
-                }
-            }
+            navigationBarItems
         }
         .onAppear {
             title = targetBook.title
         }
         .padding(.vertical)
+    }
+}
+
+extension TargetBookDetailHeaderView {
+    var navigationTitle: some View {
+        Group {
+            if scrollYOffset > 30 {
+                Text(title)
+                    .frame(width: mainScreen.width * 0.65)
+                    .lineLimit(1)
+                    .truncationMode(.middle)
+                    .navigationTitleStyle()
+            } else {
+                Text("도서 정보")
+                    .navigationTitleStyle()
+            }
+        }
+    }
+    
+    var navigationBarItems: some View {
+        HStack {
+            Button {
+                dismiss()
+            } label: {
+                Image(systemName: "chevron.left")
+                    .navigationBarItemStyle()
+            }
+            
+            Spacer()
+            
+            Menu {
+                Section {
+                    Button {
+                        // do something...
+                    } label: {
+                        Label("편집", systemImage: "square.and.pencil")
+                    }
+
+                    Button(role: .destructive) {
+                        RealmManager.shared.deleteReadingBook(targetBook.isbn13)
+                        dismiss()
+                    } label: {
+                        Label("삭제", systemImage: "trash")
+                    }
+                } header: {
+                    Text("도서 편집")
+                }
+            } label: {
+                Image(systemName: "ellipsis")
+                    .navigationBarItemStyle()
+            }
+        }
     }
 }
 
