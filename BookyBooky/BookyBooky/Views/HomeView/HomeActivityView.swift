c369de24842f9da3ed8bab0529be1c0c9659f38e
Zizi_Kim <rlarjsdn3@naver.com>
2023-05-12 10:33:10 +0900

    [file]: HomeActivityView 파일 추가

---
+++ BookyBooky/BookyBooky/Views/HomeView/HomeActivityView.swift
@@ -0,0 +1,21 @@
+//
+//  HomeActivityView.swift
+//  BookyBooky
+//
+//  Created by 김건우 on 2023/05/12.
+//
+
+import SwiftUI
+
+struct HomeActivityView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .toolbar(.hidden, for: .navigationBar)
+    }
+}
+
+struct HomeActivityView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeActivityView()
+    }
+}
