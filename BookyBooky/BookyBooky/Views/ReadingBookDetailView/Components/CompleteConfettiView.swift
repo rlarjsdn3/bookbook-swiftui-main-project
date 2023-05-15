76cfc9a82d4b796a0905e09669311815c7b39bc2
Zizi_Kim <rlarjsdn3@naver.com>
2023-05-15 14:15:32 +0900

    [file]: ComleteConfettiView 파일 추가

---
+++ BookyBooky/BookyBooky/Views/ReadingBookDetailView/Components/CompleteConfettiView.swift
@@ -0,0 +1,20 @@
+//
+//  CompleteConfettiView.swift
+//  BookyBooky
+//
+//  Created by 김건우 on 2023/05/15.
+//
+
+import SwiftUI
+
+struct CompleteConfettiView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CompleteConfettiView_Previews: PreviewProvider {
+    static var previews: some View {
+        CompleteConfettiView()
+    }
+}
