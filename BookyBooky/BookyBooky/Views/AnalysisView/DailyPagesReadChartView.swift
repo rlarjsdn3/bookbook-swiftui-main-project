5d0c0730299936e8ba402a5d066c7fd92961624c
Zizi_Kim <rlarjsdn3@naver.com>
2023-06-10 22:25:11 +0900

    [file]: DailyPagesReadChartView 파일 추가

---
+++ BookyBooky/BookyBooky/Views/AnalysisView/DailyPagesReadChartView.swift
@@ -0,0 +1,18 @@
+//
+//  DailyPagesReadChartView.swift
+//  BookyBooky
+//
+//  Created by 김건우 on 6/10/23.
+//
+
+import SwiftUI
+
+struct DailyPagesReadChartView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DailyPagesReadChartView()
+}
