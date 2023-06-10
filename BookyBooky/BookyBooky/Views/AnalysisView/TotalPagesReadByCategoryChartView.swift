10c7ba5880943d22d071aa77eef1607734072a8b
Zizi_Kim <rlarjsdn3@naver.com>
2023-06-10 20:14:14 +0900

    [file]: TotalPagesReadByCategoryChartView 파일 추가

---
+++ BookyBooky/BookyBooky/Views/AnalysisView/TotalPagesReadByCategoryChartView.swift
@@ -0,0 +1,18 @@
+//
+//  totalPagesReadByCategoryChartView.swift
+//  BookyBooky
+//
+//  Created by 김건우 on 6/10/23.
+//
+
+import SwiftUI
+
+struct totalPagesReadByCategoryChartView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    totalPagesReadByCategoryChartView()
+}
