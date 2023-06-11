6489ca63a032893b0d32c30eed9e87825100be72
Zizi_Kim <rlarjsdn3@naver.com>
2023-06-11 22:53:06 +0900

    [file]: MonthlyCompleteBookChartView 파일 추가

---
+++ BookyBooky/BookyBooky/Views/AnalysisView/monthlyCompleteBookChartView.swift
@@ -0,0 +1,18 @@
+//
+//  monthlyCompleteBookChartView.swift
+//  BookyBooky
+//
+//  Created by 김건우 on 6/11/23.
+//
+
+import SwiftUI
+
+struct monthlyCompleteBookChartView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    monthlyCompleteBookChartView()
+}
