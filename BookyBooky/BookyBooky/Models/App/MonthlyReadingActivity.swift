d795b003d63d3940574e6d2ead74bcfacfef2dc6
Zizi_Kim <rlarjsdn3@naver.com>
2023-06-15 23:49:25 +0900

    [refactor]: 코드 리팩토링

--- BookyBooky/BookyBooky/Models/App/MonthlyReadingActivity.swift
+++ BookyBooky/BookyBooky/Models/App/MonthlyReadingActivity.swift
@@ -9,5 +9,32 @@ import Foundation
 
 struct MonthlyReadingActivity: Hashable {
     var date: Date
-    var activities: [ReadingActivity]
+    var readingActivity: [ReadingActivity]
+}
+
+extension MonthlyReadingActivity {
+    var totalPagesRead: Int {
+        readingActivity.reduce(0, { $0 + $1.numOfPagesRead })
+    }
+    
+    var completeBookCount: Int {
+        readingActivity.reduce(0, { $1.isComplete ? $0 + 1 : $0 })
+    }
+    
+    var readingDayCount: Int {
+        var date: [Date] = []
+        
+        for activity in self.readingActivity {
+            if !date.contains(
+                where: { $0.isEqual([.year, .month, .day], date: activity.date) }
+            ) {
+                date.append(activity.date)
+            }
+        }
+        return date.count
+    }
+    
+    var averagePagesRead: Int {
+        totalPagesRead / readingDayCount
+    }
 }
